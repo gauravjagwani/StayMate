e6c62ff356c73b9006d5ec5b4f28710d1ab5f744
gauravjagwani <gauravjagwani8@gmail.com>
2024-10-20 01:07:14 +0530

    middleware to handle errors

--- backend/index.js
+++ backend/index.js
@@ -28,3 +28,14 @@ app.listen(4000, () => {
 });
 
 app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
